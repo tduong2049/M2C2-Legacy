6047525c916021336b317f0dc118c4bed31e5f2d
Tri Duong <tduong2049@gmail.com>
2019-05-01 15:39:14 -0400

    Completed Agent's Affiliation Activity

--- M2C2/app/src/main/java/com/example/m2_c2/AffiliationSetupActivity.kt
+++ M2C2/app/src/main/java/com/example/m2_c2/AffiliationSetupActivity.kt
@@ -3,12 +3,16 @@ package com.example.m2_c2
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.RecyclerView
-import android.widget.Button
-import android.widget.TextView
+import android.view.View
+import android.widget.*
+import org.jetbrains.anko.find
 
 class AffiliationSetupActivity : AppCompatActivity() {
-    lateinit var affiliationRecycler: RecyclerView
-    lateinit var selectVillain: Button
+    lateinit var affiliationOneSpinner: Spinner
+    lateinit var affiliationTwoSpinner: Spinner
+    lateinit var teamUpSwitch: Switch
+    lateinit var affiliationTwoText: TextView
+    lateinit var selectVillainButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +27,62 @@ class AffiliationSetupActivity : AppCompatActivity() {
         agentClassText.text = "Class: " + agent.name
         agentSexText.text = "Sex: " + agent.sex
         agentRankText.text = "Rank: " + agent.rank.toString()
+
+        // Find the ID of each spinner for choosing the Agent's affiliations and store them in their own variables
+        affiliationOneSpinner = findViewById(R.id.affiliatonOne_spinner)
+        affiliationTwoSpinner = findViewById(R.id.affiliatonTwo_spinner)
+
+        // Create adapters to grab available options and display them in their corresponding spinners
+        ArrayAdapter.createFromResource(this, R.array.affiliation_array, android.R.layout.simple_spinner_dropdown_item).also {
+                adapter -> adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            affiliationOneSpinner.adapter = adapter
+        }
+
+        ArrayAdapter.createFromResource(this, R.array.affiliation_array, android.R.layout.simple_spinner_dropdown_item).also {
+                adapter -> adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            affiliationTwoSpinner.adapter = adapter
+        }
+
+        affiliationTwoText = findViewById(R.id.affiliationTwo_text)
+
+        teamUpSwitch = findViewById(R.id.teamUp_switch)
+
+        teamUpSwitch.setOnClickListener {
+            if (teamUpSwitch.isChecked) {
+                affiliationTwoText.visibility = View.VISIBLE
+                affiliationTwoSpinner.visibility = View.VISIBLE
+            }
+
+            else {
+                affiliationTwoText.visibility = View.GONE
+                affiliationTwoSpinner.visibility = View.GONE
+            }
+        }
+
+        selectVillainButton = findViewById(R.id.selectVillian_button)
+
+        selectVillainButton.setOnClickListener {
+            affiliationChoiceChecker(agent, affiliationOneSpinner, affiliationTwoSpinner, teamUpSwitch)
+        }
+    }
+
+    private fun affiliationChoiceChecker(agent: Agent, affiliationOneSpinner: Spinner, affiliationTwoSpinner: Spinner,
+                                         teamUpSwitch: Switch) {
+        agent.affiliations.clear()
+
+        val affiliationOneName: String = affiliationOneSpinner.selectedItem.toString()
+        val affiliationTwoName: String = affiliationTwoSpinner.selectedItem.toString()
+
+        if (affiliationOneName.length > 1) {
+            val affiliation01 = Affiliation(affiliationOneName)
+            agent.affiliations.add(affiliation01)
+        }
+
+        if (affiliationTwoName.length > 1) {
+            val affiliation02 = Affiliation(affiliationTwoName)
+            agent.affiliations.add(affiliation02)
+        }
+
+        if (teamUpSwitch.isChecked && (agent.affiliations.size == 1)) { agent.affiliations[0].hasDisadvantages = false }
     }
 }
--- M2C2/app/src/main/java/com/example/m2_c2/Card.kt
+++ M2C2/app/src/main/java/com/example/m2_c2/Card.kt
@@ -5,39 +5,29 @@ import java.io.Serializable
 open class Card(val name: String = "Name", var isSelected: Boolean = false) : Serializable {
 }
 
-class Agent(name: String, val sex: String = "Male", var rank: Int = 1,
-                var strength: Int = 0) : Card(name) {
+class Agent(name: String, val sex: String = "Male", var rank: Int = 1, var strength: Int = 0) : Card(name) {
         val equipment: MutableList<Equipment> = mutableListOf()
         val allies: MutableList<Ally> = mutableListOf()
         val powers: MutableList<Power> = mutableListOf()
-        val affiliations: MutableList<Affiliation> = mutableListOf()
+        var affiliations: MutableList<Affiliation> = mutableListOf()
+        val oneShots: MutableList<OneShot> = mutableListOf()
 }
 
-class Equipment(name: String, type: String, var bonus: Int = 0, val goldValue: Int = 0) : Card(name) {
+class Equipment(name: String, type: String, var bonus: Int = 0, val goldValue: Int = 0) : Card(name)
+
+class Ally(name: String, var bonus: Int = 0) : Card(name)
+
+class OneShot(name: String, var playerBonus: Int = 0, var monsterBonus: Int = 0, val goldValue: Int = 0) : Card(name)
+
+class Monster(name:String, val level: Int = 1, var strength: Int = 0): Card(name) {
+    val modifiers: MutableList<MonsterModifier> = mutableListOf()
+    val oneShots: MutableList<OneShot> = mutableListOf()
 }
 
-class Ally(name: String, var bonus: Int = 0) : Card(name) {
-}
+class Affiliation(name: String, var hasDisadvantages: Boolean = true) : Card(name)
 
-class OneShot(name: String, var playerBonus: Int = 0, var monsterBonus: Int = 0, val goldValue: Int = 0) : Card(name) {
-}
+class Power(name: String, val bonus: Int = 0, var rank: Int = 1) : Card(name)
 
-class Monster(name:String, val level: Int = 1, val badStuffText: String, var strength: Int, var levelGain: Int = 1):
-    Card(name) {
-}
+class MonsterModifier(name: String, val strengthModifier: Int = 0) : Card(name)
 
-class Affiliation(name: String) : Card(name) {
-
-}
-
-class Power(name: String, val bonus: Int = 0, var rank: Int = 1) : Card(name) {
-
-}
-
-class MonsterModifier(name: String, val strengthModifier: Int = 0) : Card(name) {
-
-}
-
-class Trap(name: String) : Card(name) {
-
-}
+class Trap(name: String) : Card(name)
